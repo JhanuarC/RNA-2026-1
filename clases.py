29cd6e888d0f160b0debe67b0c395d9486653e66
JhanuarC <jhcastrol@unal.edu.co>
2026-03-18 17:24:13 -0500

    Funcion de rosenbrock y schwefel completas
    Las 2 funciones completas con visualización en 2d y 3d junto con los óptimos encontrados usando el descenso del gradiente.

--- clases.py
+++ clases.py
@@ -123,8 +123,10 @@ class G_schwefel:#Clase para la función de Schwefel, usando metodos descenso po
         self.historia_2d = []
         self.historia_3d = []
     
-    def evaluate(self,x):   
-        return 418.9829 * len(x) - np.sum(x * np.sin(np.sqrt(np.abs(x))))
+    def evaluate(self,x):
+        # Si x es un vector (optimización), axis=0 suma sus elementos.
+        # Si x es una malla (gráfica), axis=0 suma las matrices correspondientes.
+        return 418.9829 * len(x) - np.sum(x * np.sin(np.sqrt(np.abs(x))),axis=0)
     
     def callback_2d(self,xk):
         self.historia_2d.append(self.evaluate(xk))
@@ -151,7 +153,7 @@ class G_schwefel:#Clase para la función de Schwefel, usando metodos descenso po
         print(f"Evaluaciones reales de la función: {res_3d.nfev}")
 
     # Graficar la evolución
-    def graficar(self):
+    def graficar_evo(self):
         plt.figure(figsize=(10, 5))
         plt.plot(self.historia_2d, label='schwefel 2D')
         plt.plot(self.historia_3d, label='schwefel 3D')
@@ -162,14 +164,73 @@ class G_schwefel:#Clase para la función de Schwefel, usando metodos descenso po
         plt.legend()
         plt.grid(True)
         plt.show()
+    
+    def grafica_3d(self,res_3d):
+        x = np.linspace(-500, 500, 250)
+        y = np.linspace(-500, 500, 250)
+        X, Y = np.meshgrid(x, y)
+        Z = self.evaluate(np.array([X, Y]))
+
+        fig = plt.figure(figsize=(12, 8))
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(X, Y, Z, cmap=cm.viridis, antialiased=False, alpha=0.8)
+
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+
+        ax.set_title('Función de Schwefel en 3D')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Schwefel(X, Y)')
+        plt.show()
+
+        try:
+            ax.scatter(res_3d.x[0], res_3d.x[1], self.evaluate(res_3d.x), color='red', s=100, label='Óptimo encontrado', marker='*')
+            ax.legend()
+        
+        except Exception as e:
+            print(f"No se pudo graficar el punto óptimo: {e}")
+
+    def graficar_2d(self,res_2d):
+        x = np.linspace(-500, 500, 250)
+        y = np.linspace(-500, 500, 250)
+        X, Y = np.meshgrid(x, y)
+        Z = self.evaluate(np.array([X, Y]))
+
+        plt.figure(figsize=(8, 6))
+        
+        cp = plt.contour(X, Y, Z, levels=np.logspace(1, 5, 20), cmap='magma')
+        plt.colorbar(cp)
+        plt.title('Curvas de Nivel - Schwefel')
+        
+        plt.plot(420.9687, 420.9687, 'go', markersize=15, label='Mínimo Global (420.9687,420.9687)',zorder =4)#Optimo global conocido de la función de Schwefel
+    
+
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.legend()
+        plt.grid(alpha=0.3)
+    
+        # Graficar el punto óptimo encontrado
+        try:
+            plt.scatter(res_2d.x[0], res_2d.x[1], color='red', s=100, label='Óptimo encontrado', marker='*', zorder =5)
+            plt.legend()
+        except Exception as e:
+            print(f"No se pudo graficar el punto óptimo: {e}")
+
+        plt.show()
+
 
     def ejecutar(self):
         res_2d = self.optimizar_2d()
         res_3d = self.optimizar_3d()
         self.resultados(res_2d, res_3d)
-        self.graficar()
-
+        self.graficar_evo()
+        self.grafica_3d(res_3d)
+        self.graficar_2d(res_2d)
 
 
 rosen = G_rosenbrock()
+rosen.ejecutar()
+
+rosen = G_schwefel()
 rosen.ejecutar()
